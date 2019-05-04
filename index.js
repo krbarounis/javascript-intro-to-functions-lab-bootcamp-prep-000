6f89c8206652c9d2ba3f7040811bda947e03d821
Kristina Barounis <krbarounis@gmail.com>
2019-05-04 21:24:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,9 @@ function sayHiToGrandma(string)
 {
   if (string === string.toLowerCase()) {
     return "I can't hear you"
-    else 
   }
-  else return sti
+    else if (string === string.toUpperCase()) {
+      return "YES INDEED!"
+    }
 }
+ 
